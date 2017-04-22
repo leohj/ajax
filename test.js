ccaccfe81687c9293bf05119b3502ab256352d80
leohj <hyphnox@gmail.com>
2017-04-21 19:58:57 -0500

    prueba

---
+++ test.js
@@ -0,0 +1,20 @@
+//Manipula la data para imprimirla
+$.getJSON("heroes.json",function(data){
+	$.each( data.heroes, function( a, b ) {
+		var urlimage="img/"+ b.name+"_hphover.png"
+		var content="";
+		content+="<div class=\"profile\">";
+		content+="	<img src="+urlimage+">";
+		content+="	<span>"+b.localized_name+"</span>";
+		content+="</div>"
+		$( "#demo" ).append(content);
+
+/*		$( "<div>" ).attr( "class", "profile").attr.("data-value",b.localized_name).appendTo( "#demo" );
+
+		$( "<img>" ).attr( "src", tmp).appendTo( "#demo" );
+		$( "#demo" ).append( "<span class=texto>"+b.localized_name+"</span>" );
+		console.log(b.localized_name);
+	*/
+	});
+
+});
